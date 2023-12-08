6b671e1f692b74471d3debdd383fb00ba80cf0d0
Ttunay <tigrus232323k@gmail.com>
2023-12-08 18:29:13 +0300

    stack

--- calc.c
+++ calc.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main()
+struct addr 
 {
-    
+	char name[30];
+	char street[40];
+	char city[20];
+	char state[3];
+	unsigned long int zip;
+};
+
+	struct addr addr_info;
+
+int main(void)
+{
+
+		scanf("%s'n",addr_info.name);
+
+	printf("NAME == %s\n",addr_info.name);
 }
